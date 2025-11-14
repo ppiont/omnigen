6006994884dcafa46d82c3418adc5872ac68cfdd
Akhil Pinnani <akhilrvpinnani@gmail.com>
2025-11-14 17:59:21 -0600

    fix: hide navbar on dashboard page
    - Navbar only shows on Landing, Login, and Signup pages
    - Dashboard uses its own layout without the top navbar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/Navbar'
 import Landing from './pages/Landing'
@@ -6,18 +6,27 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import { DashboardPage } from './pages/Dashboard'
 
+function AppContent() {
+  const location = useLocation()
+  const showNavbar = location.pathname !== '/dashboard'
+
+  return (
+    <div className="app-shell">
+      {showNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </div>
+  )
+}
+
 function App() {
   return (
     <ThemeProvider>
-      <div className="app-shell">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-        </Routes>
-      </div>
+      <AppContent />
     </ThemeProvider>
   )
 }
